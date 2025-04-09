014ac0704d764978e4100e3fa2da91da038e810d
zhengri <rzheng@quinnipiac.edu>
2025-04-09 02:04:24 -0400

    MVP
    Multiplayer, Player movement rotation, push ability modification, koth multiplayer modification

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,46 +6,78 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public PlayerInputManager playerInputManager;
-    private List<Player> playerList = new List<Player>();
-    [SerializeField] private Player playerPrefab1;
-    [SerializeField] private Player playerPrefab2;
-    [SerializeField] private Player playerPrefab3;
-    [SerializeField] private Player playerPrefab4;
-    private Vector3 player1Transform;
-    private Vector3 player2Transform;
-    private Vector3 player3Transform;
-    private Vector3 player4Transform;
 
+    private List<Player> playerList = new List<Player>();
+    [SerializeField] private List<GameObject> playerPrefabs = new List<GameObject>();
+    [SerializeField] private GameObject playerPrefab;
+    //[SerializeField] private GameObject player1Spawn;
+    //[SerializeField] private GameObject player2Spawn;
+    //[SerializeField] private GameObject player3Spawn;
+    //[SerializeField] private GameObject player4Spawn;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        playerTotal();
+    }
+
+    //eventually rework the multiplayer system to use manual join and the JoinPlayer() method of the playerinputmanager. this will just pair devi
     void Update()
     {
+        playerPrefab = PlayerInputManager.instance.playerPrefab = playerPrefabs[playerInputManager.playerCount];
         playerInputManager.DisableJoining();
         switch (GameManager.Instance.GameState)
         {
             case (GameManager.GameStateEnums.SelectPlayers):
                 playerInputManager.EnableJoining();
                 break;
+            //case (GameManager.GameStateEnums.InGame):
+            //    playerInputManager.EnableJoining();
+            //    break;
             case (GameManager.GameStateEnums.InGame):
+                playerInputManager.EnableJoining();
+                //spawn player prefab based off player count in game
                 switch (playerInputManager.playerCount)
                 {
-                    case (0):
-                        Instantiate(playerPrefab1, player1Transform, Quaternion.identity);
-                        break;
                     case (1):
-                        Instantiate(playerPrefab2, player2Transform, Quaternion.identity);
+                        PlayerInputManager.instance.playerPrefab = playerPrefab;
+                        //Instantiate(playerPrefab1, player1Spawn.transform.position, Quaternion.identity);
+                        playerTotal();
                         break;
                     case (2):
-                        Instantiate(playerPrefab3, player3Transform, Quaternion.identity);
+                        PlayerInputManager.instance.playerPrefab = playerPrefab;
+                        //Instantiate(playerPrefab2, player2Spawn.transform.position, Quaternion.identity);
+                        playerTotal();
                         break;
                     case (3):
-                        Instantiate(playerPrefab4, player4Transform, Quaternion.identity);
+                        PlayerInputManager.instance.playerPrefab = playerPrefab;
+                        //Instantiate(playerPrefab3, player3Spawn.transform.position, Quaternion.identity);
+                        playerTotal();
+                        break;
+                    case (4):
+                        PlayerInputManager.instance.playerPrefab = playerPrefab;
+                        //Instantiate(playerPrefab4, player4Spawn.transform.position, Quaternion.identity);
+                        playerTotal();
+                        break;
+                    default:
                         break;
 
                 }
                 break;
+
+
         }
-
-
-
-
     }
+    public void playerTotal()
+    {
+        //Debug.Log("total player number: " + playerInputManager.playerCount);
+    }
+
+
+
 }
+
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,22 +5,25 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [Header("Movement")]
-    [SerializeField] private float speed;
-    [SerializeField] private float acceleration;
-    [SerializeField] private float deceleration;
-    [SerializeField] private float maxAcceleration;
+    [SerializeField] private float speed = 100;
     [SerializeField] private float horizontalInput;
     [SerializeField] private float verticalInput;
+    [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] private Vector3 forceDirection = new Vector3(1f, 0f, 1f).normalized;
 
     [Header("Jump")]
     [SerializeField] private bool isGrounded;
     [SerializeField] private float jumpValue = 5f;
 
     [Header("Knockback")]
-    [SerializeField] private float knockValue = 5f;
-    [SerializeField] private float knockRange = 2f;
+    [SerializeField] private float knockbackValue;
+    [SerializeField] private Vector3 knockupVector = new Vector3(0,10,0);
+    [SerializeField] private Vector3 boxSize = new Vector3(3,3,3);
+    [SerializeField] private float boxDistance = 2f;
     [SerializeField] private float pushCooldown = 1f;
     [SerializeField] private float cooldownRemaining = 1f;
+
+    [SerializeField] private Vector3 boxCenter;
 
 
     [Header("Rigidbody/Input")]
@@ -46,6 +49,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        boxCenter = transform.position + transform.forward * boxDistance;
         horizontalInput = inputManager.movementVector.x;
         verticalInput = inputManager.movementVector.y;
         cooldownRemaining -= Time.deltaTime;
@@ -71,13 +75,24 @@ public class Player : MonoBehaviour
     public void FixedUpdate()
     {
         Move();
+        
     }
     public void Move()
     {
         Vector3 movement = new Vector3 (horizontalInput,0, verticalInput);
-        rb.linearVelocity = movement * speed;
+        rb.AddForce(movement* speed);
+        //rb.linearVelocity = movement * speed;
+
         Vector3 friction = new Vector3(-rb.linearVelocity.x, -2f, -rb.linearVelocity.z);
+        //rotate the player towards the velocity
+        if (rb.linearVelocity.sqrMagnitude > 0.01f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(rb.linearVelocity.normalized);
+            rb.MoveRotation(Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f));
+        }
+        
         rb.AddForce(friction * speed * 0.1f);
+
 
     }
 
@@ -92,21 +107,36 @@ public class Player : MonoBehaviour
     //push ability
     public void Push()
     {
+        Debug.Log("pushed");
         if (cooldownRemaining <= 0) {
             cooldownRemaining -= Time.deltaTime;
-            Collider[] hits = Physics.OverlapSphere(transform.position, knockRange);
+            Collider[] hits = Physics.OverlapBox(boxCenter, boxSize/2 ,transform.rotation);
 
             foreach (Collider hit in hits)
             {
                 if (hit.CompareTag("Player"))
                 {
-                    Vector3 direction = (hit.transform.position - transform.position).normalized;
+                    Vector3 direction = (hit.transform.position - transform.position).normalized + knockupVector ;
                     Rigidbody rb = hit.GetComponent<Rigidbody>();
-                    rb.AddForce(direction * knockValue, ForceMode.Impulse);
+                    rb.AddForce(direction * knockbackValue, ForceMode.Impulse);
                 }
             }
             cooldownRemaining = pushCooldown;
         }
         
     }
+    void DebugDrawBox(Vector3 center, Vector3 size, Quaternion rotation, Color color)
+    {
+        Matrix4x4 matrix = Matrix4x4.TRS(center, rotation, size);
+        Gizmos.color = color;
+        Gizmos.matrix = matrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+
+    void OnDrawGizmos()
+    {
+        Vector3 boxCenter = transform.position + transform.forward * boxDistance;
+        DebugDrawBox(boxCenter, boxSize, transform.rotation, Color.green);
+    }
+
 }
