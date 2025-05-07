43bbc867c52d3ed85fc7fee6591d74b3043f3dfc
Nicholas Larkin <nglarkin@quinnipiac.edu>
2025-05-06 21:56:06 -0400

    weird merge stash fix

--- Assets/Scripts/Managers/DeathTrigger.cs
+++ Assets/Scripts/Managers/DeathTrigger.cs
@@ -18,12 +18,9 @@ public class DeathTrigger : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-<<<<<<< Updated upstream
-=======
             //playerDeathOrder.Add(PlayerManager.playerList.FindIndex(go => go == other.gameObject));
             SoundManager.Instance.PlaySFXWithSource(12, 1f); //death sfx
             Debug.Log("DeathOrderList: " + DeathTrigger.playerDeathOrder);
->>>>>>> Stashed changes
             other.gameObject.GetComponent<Player>().playerAlive = false;
             other.gameObject.GetComponent<Transform>().position = deathPos;
             PlayerManager.playerStatus[other.gameObject.GetComponent<Player>().playerNumber] = false;
